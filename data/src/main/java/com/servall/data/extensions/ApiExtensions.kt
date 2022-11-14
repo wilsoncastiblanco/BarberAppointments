c369fda33503fedc94c8df590008ffedf6a29d56
wilson.castiblanco@avantica.net <WwCc135$>
2022-11-14 12:01:31 -0600

    Created a generic type to handle Api Responses o be re-used in later implementations

---
+++ data/src/main/java/com/servall/data/extensions/ApiExtensions.kt
@@ -0,0 +1,28 @@
+package com.servall.data.extensions
+
+import com.servall.data.exceptions.NullBodyException
+import com.servall.domain.entities.Response
+import retrofit2.Response as ApiResponse
+
+fun <E : Any, T : Any> ApiResponse<T>.handleApiResponse(map: (body: T) -> E): Response<E> {
+    return if (this.isSuccessful) {
+        val body = this.body()
+        if (body != null) {
+            Response.Success(map(body))
+        } else {
+            Response.Error(NullBodyException())
+        }
+    } else {
+        Response.Error(
+            Exception("The server has returned an invalid response. ${this.errorBody().toString()}")
+        )
+    }
+}
+
+suspend fun <T : Any> safeApiCall(call: suspend () -> Response<T>): Response<T> {
+    return try {
+        call()
+    } catch (e: Exception) {
+        Response.Error(e)
+    }
+}
--- data/src/main/java/com/servall/data/remote/LoginRemoteRepository.kt
+++ data/src/main/java/com/servall/data/remote/LoginRemoteRepository.kt
@@ -2,7 +2,8 @@ package com.servall.data.remote
 
 import com.servall.data.encrypt
 import com.servall.data.entities.LoginDto
-import com.servall.data.exceptions.NullBodyException
+import com.servall.data.extensions.handleApiResponse
+import com.servall.data.extensions.safeApiCall
 import com.servall.data.toModel
 import com.servall.domain.entities.Response
 import com.servall.domain.entities.User
@@ -15,25 +16,14 @@ class LoginRemoteRepository(
 ) : LoginRepository {
 
     override suspend fun login(userName: String, password: String): Response<User> {
-        try {
+        return safeApiCall {
             val response = api.login(
                 LoginDto(
                     userName = userName,
                     password = sha256Generator.encrypt(password.toByteArray())
                 )
             )
-            return if (response.isSuccessful) {
-                val body = response.body()
-                if (body != null) {
-                    Response.Success(body.toModel())
-                } else {
-                    Response.Error(NullBodyException())
-                }
-            } else {
-                Response.Error(Exception("The server has returned an invalid response. ${response.errorBody().toString()}"))
-            }
-        } catch (e: Exception) {
-            return Response.Error(e)
+            response.handleApiResponse { body -> body.toModel() }
         }
     }
 
